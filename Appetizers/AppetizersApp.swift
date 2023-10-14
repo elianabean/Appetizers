1b19766fd17f6588ce440b60cecc2f8753cf7001
Eliana <ewanginspiration@gmail.com>
2023-10-14 12:46:06 -0400

    Initial Commit

---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by Eliana Wang on 10/14/23.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
