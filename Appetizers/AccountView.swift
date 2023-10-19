35d230a97518c12e714ce4ec653b29ceac57e814
Eliana <ewanginspiration@gmail.com>
2023-10-19 19:01:24 -0400

    added AccountView

--- Appetizers/AccountView.swift
+++ Appetizers/AccountView.swift
@@ -8,10 +8,46 @@
 import SwiftUI
 
 struct AccountView: View {
+    
+    @State private var firstName = ""
+    @State private var lastName = ""
+    @State private var email = ""
+    @State private var birthdate = Date()
+    @State private var extraNapkins = false;
+    @State private var frequentRefills = false;
+    
     var body: some View {
         NavigationView {
-            Text("Account View")
-                .navigationTitle("🤣 Account")
+            Form {
+                Section(header: Text("Personal Info")) {
+                        TextField("First Name", text: $firstName) //$ to bind the variable
+                        TextField("Last Name", text: $lastName)
+                        TextField("Email", text: $email)
+                            .keyboardType(.emailAddress)
+                            .autocapitalization(.none)
+                            .disableAutocorrection(true)
+                    DatePicker("Birthday", selection: $birthdate, displayedComponents: .date)
+                    
+                    Button {
+                        print("Save")
+                    } label: {
+                        Text("Save Changes")
+                    }
+                }
+                
+                
+                Section(header: Text("Requests")) {
+                    Toggle("Extra Napkins", isOn: $extraNapkins)
+                        .toggleStyle(SwitchToggleStyle(tint: Color("brandPrimary")))
+                    
+                    Toggle("Frequent Refills", isOn: $frequentRefills)
+                    }
+                
+                
+                
+            }
+            .navigationTitle("🤣 Account")
+            
         }
     }
 }
